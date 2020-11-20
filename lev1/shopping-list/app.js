048c55e663bc9071e1fd29b652af746921d40300
Isa Abutaa <isaabutaa@gmail.com>
2020-11-20 14:10:46 -0500

    finished shopping list assignment - user can add and delete items

--- lev1/shopping-list/app.js
+++ lev1/shopping-list/app.js
@@ -1,18 +1,47 @@
 const header = document.getElementById("header")
+header.classList.add("title")
 header.textContent = "Shopping List"
 
-const userInput = document.getElementsByTagName("input")[0].value
 const addBtn = document.getElementById("add-btn")
-const shoppingList = document.getElementById("shopping-list")
 
-function addListItem() {
+function addListItem(e) {
+    e.preventDefault()
+
+    let userInput = document.getElementById("item").value
+    const shoppingList = document.getElementById("shopping-list")
     const newItem = document.createElement("li")
-    newItem.textContent = userInput
+    const completeBtn = document.createElement("button")
+    const deleteBtn = document.createElement("button")
+
+    completeBtn.textContent = "complete"
+    completeBtn.style.backgroundColor = "green"
+    completeBtn.style.color = "#ffffff"
+    completeBtn.style.margin = "0 5px"
+
+    deleteBtn.textContent = "delete"
+    deleteBtn.style.backgroundColor = "firebrick"
+    deleteBtn.style.color = "#ffffff"
+
+    newItem.style.listStyleType = "none"
+    newItem.textContent = userInput + "  "
+
+    newItem.append(completeBtn)
+    newItem.append(deleteBtn)
     shoppingList.append(newItem)
+    document.getElementById("item").value = ""
+
+    function crossOutItem() {
+        newItem.style.textDecorationLine = "line-through"
+    }
+
+    function deleteItem() {
+        shoppingList.removeChild(newItem)
+    }
+
+    completeBtn.addEventListener("click", crossOutItem)
+
+    deleteBtn.addEventListener("click", deleteItem)
 }
 
-addBtn.addEventListener("click", (e) => {
-    e.preventDefault()
-    console.log(userInput)
-})
+addBtn.addEventListener("click", addListItem)
 
