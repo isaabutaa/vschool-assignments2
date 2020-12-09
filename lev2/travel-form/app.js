881f266bef2b59881520da52d5e271e4cf34ec21
Isa Abutaa <isaabutaa@gmail.com>
2020-12-09 14:56:41 -0500

    basic travel form

---
+++ lev2/travel-form/app.js
@@ -0,0 +1,21 @@
+const form = document.form
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    // values
+    const firstName = form.firstName.value
+    const lastName = form.lastName.value
+    const gender = form.gender.value
+    const destination = form.destination.value
+    const diet = []
+    
+    for(let i = 0; i < form.diet.length; i++) {
+        if(form.diet[i].checked) {
+            diet.push(form.diet[i].value)
+        }
+    }
+
+    console.log(firstName, lastName, gender, destination, diet)
+
+})
