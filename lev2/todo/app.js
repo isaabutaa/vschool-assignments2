6bec2111b41cf62c0e616d0d1af7a75f05a12e62
Isa Abutaa <isaabutaa@gmail.com>
2020-12-27 15:31:36 -0500

    user can now add, edit, delete, and get all to-do items

--- lev2/todo/app.js
+++ lev2/todo/app.js
@@ -1,35 +1,17 @@
-// get all todos
-function getTodos() {
-    
-    axios.get("https://api.vschool.io/ragnarlothbrok/todo")
-    .then(res => {
-            const todoItems = document.getElementById("todo-items")
-
-            for(let i = 0; i < res.data.length; i++) {
-                const todoCard = document.createElement("div")
-                todoCard.className = "todo-card"
-                const todoTitle = document.createElement("h3")
-                todoTitle.className = "todo-title"
-                const todoDescription = document.createElement("p")
-                todoDescription.className = "todo-description"
-                const editBtn = document.createElement("button")
-                editBtn.textContent = "Edit"
-                editBtn.className = "btn edit-btn"
-                todoTitle.textContent = res.data[i].title
-                todoDescription.textContent = res.data[i].description
-                todoCard.appendChild(todoTitle)
-                todoCard.appendChild(todoDescription)
-                todoCard.appendChild(editBtn)
-                todoItems.appendChild(todoCard)
-                
-            }
-        })
-        .catch(err => console.log(err))
-}
-
 getTodos()
 
-// add new todos
+document.todoForm.addEventListener("submit", addTodo)
+
+// get todos function
+function getTodos() {
+    axios.get("https://api.vschool.io/ragnarlothbrok/todo")
+    .then(res => {
+        createTodos(res)
+    })
+    .catch(err => console.log(err))
+}
+
+// add new todos function for form
 
 function addTodo(e) {
     e.preventDefault()
@@ -41,13 +23,125 @@ function addTodo(e) {
 
     axios.post("https://api.vschool.io/ragnarlothbrok/todo", newTodo)
         .then(res => {
-            const newItem = document.createElement("p")
-            newItem.textContent = res.data.title
-            document.getElementById("todo-items").appendChild(newItem)
+            document.getElementById("todo-items").innerHTML = ""
+            getTodos()
         })
     
     todoForm.title.value = ""
     todoForm.description.value = ""
 }
 
-document.todoForm.addEventListener("submit", addTodo)
+// create todos
+function createTodos(res) {
+    for(let i = 0; i < res.data.length; i++) {
+        const todoItems = document.getElementById("todo-items")
+        const todoCard = document.createElement("div")
+        todoCard.className = "todo-card"
+
+        const todoTitle = document.createElement("h3")
+        todoTitle.className = "todo-title"
+        todoTitle.textContent = res.data[i].title
+
+        const todoDescription = document.createElement("p")
+        todoDescription.className = "todo-description"
+        todoDescription.textContent = res.data[i].description
+
+        const editBtn = document.createElement("button")
+        editBtn.textContent = "Edit"
+        editBtn.className = "btn edit-btn"
+
+        const deleteBtn = document.createElement("button")
+        deleteBtn.textContent = "Delete"
+        deleteBtn.className = "btn delete-btn"
+
+        const completeBtn = document.createElement("button")
+        completeBtn.textContent = "Mark Complete"
+        completeBtn.className = "btn complete-btn"
+        
+        todoCard.appendChild(todoTitle)
+        todoCard.appendChild(todoDescription)
+        todoCard.appendChild(editBtn)
+        todoCard.appendChild(deleteBtn)
+        todoCard.appendChild(completeBtn)
+        todoItems.appendChild(todoCard)
+
+        if(res.data[i].completed) {
+            todoCard.style.backgroundColor = "gold"
+            completeBtn.style.display = "none"
+        }
+
+        // edit todos function
+        editBtn.addEventListener("click", () => {
+            todoTitle.style.display = "none"
+            todoDescription.style.display = "none"
+            editBtn.style.display = "none"
+
+            const editForm = document.createElement("form")
+            editForm.className = "edit-form"
+            todoCard.appendChild(editForm)
+            
+            const titleEdit = document.createElement("input")
+            titleEdit.value = todoTitle.textContent
+            editForm.appendChild(titleEdit)
+
+            const descriptionEdit = document.createElement("input")
+            descriptionEdit.value = todoDescription.textContent
+            editForm.appendChild(descriptionEdit)
+
+            const submitEditBtn = document.createElement("button")
+            submitEditBtn.textContent = "Submit Edit"
+            submitEditBtn.className = "btn submit-edit-btn"
+            editForm.appendChild(submitEditBtn)
+
+            // submit Edit function
+            editForm.addEventListener("submit", (e) => {
+                e.preventDefault()
+
+                const editedTodo = {
+                    title: titleEdit.value,
+                    description: descriptionEdit.value
+                }
+
+                axios.put(`https://api.vschool.io/ragnarlothbrok/todo/${res.data[i]._id}`, editedTodo)
+                    .then(res => {
+                        todoTitle.style.display = "block"
+                        todoDescription.style.display = "block"
+                        editBtn.style.display = "block"
+
+                        editForm.style.display = "none"
+
+                        document.getElementById("todo-items").innerHTML = ""
+
+                        getTodos()
+                    })
+                    .catch(err => console.log(err))
+            })
+        })
+
+        // delete todo function
+        deleteBtn.addEventListener("click", () => {
+            axios.delete(`https://api.vschool.io/ragnarlothbrok/todo/${res.data[i]._id}`)
+                .then(res => {
+                    document.getElementById("todo-items").innerHTML = ""
+                    getTodos()
+                })
+                .catch(err => console.log(err))
+        })
+
+        // mark item as complete function
+        completeBtn.addEventListener("click", () => {
+            const completedTodo = {
+                completed: true
+            }
+
+            axios.put(`https://api.vschool.io/ragnarlothbrok/todo/${res.data[i]._id}`, completedTodo)
+                .then(res => {
+                    console.log(res.data)
+                    todoCard.style.backgroundColor = "gold"
+                    completeBtn.style.display = "none"
+                })
+                .catch(err => console.log(err))
+        })
+    }
+}
+
