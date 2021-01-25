e229c51dc866c551decbe80fa861f2f7dc67cac8
Isa Abutaa <isaabutaa@gmail.com>
2021-01-25 16:22:57 -0500

    created user context

--- lev6/token-auth-pt2/client/src/context/UserProvider.js
+++ lev6/token-auth-pt2/client/src/context/UserProvider.js
@@ -1,1 +1,18 @@
-import React from "react"
+import React, {useState} from "react"
+
+export const userContext = React.createContext()
+
+export default function UserProvider(props) {
+    const initState = { 
+        user: {}, 
+        token: "", 
+        todos: [] 
+    }
+    const [userState, setUserState] = useState(initState)
+
+    return (
+        <userContext.Provider value={{...userState}}>
+            { props.children }
+        </userContext.Provider>
+    )
+}
