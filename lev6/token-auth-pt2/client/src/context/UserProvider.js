bce98272154d6ca0fd6bab3805ae3088396ba3d4
Isa Abutaa <isaabutaa@gmail.com>
2021-01-25 20:49:51 -0500

    created signup and login functions to save user credentials

--- lev6/token-auth-pt2/client/src/context/UserProvider.js
+++ lev6/token-auth-pt2/client/src/context/UserProvider.js
@@ -1,18 +1,55 @@
 import React, {useState} from "react"
+import axios from 'axios'
 
-export const userContext = React.createContext()
+export const UserContext = React.createContext()
 
 export default function UserProvider(props) {
     const initState = { 
-        user: {}, 
-        token: "", 
+        user: JSON.parse(localStorage.getItem("user")) || {}, 
+        token: localStorage.getItem("token") || "", 
         todos: [] 
     }
     const [userState, setUserState] = useState(initState)
 
+    function signup(credentials) {
+        axios.post("/auth/signup", credentials)
+            .then(res => {
+                const { user, token } = res.data
+                localStorage.setItem("user", JSON.stringify(user))
+                localStorage.setItem("token", token)
+                setUserState(prevState => ({
+                    ...prevState,
+                    user,
+                    token
+                }))
+            })
+            .catch(err => console.log(err.response.data.errMsg))
+    }
+
+    function login(credentials) {
+        axios.post("/auth/login", credentials)
+            .then(res => {
+                const { user, token } = res.data
+                localStorage.setItem("user", JSON.stringify(user))
+                localStorage.setItem("token", token)
+                setUserState(prevState => ({
+                    ...prevState,
+                    user,
+                    token
+                }))
+            })
+            .catch(err => console.log(err.response.data.errMsg))
+    }
+
     return (
-        <userContext.Provider value={{...userState}}>
+        <UserContext.Provider 
+            value={{
+                ...userState,
+                signup,
+                login
+            }}
+        >
             { props.children }
-        </userContext.Provider>
+        </UserContext.Provider>
     )
 }
