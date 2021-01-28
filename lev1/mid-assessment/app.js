cff9b3ed50b6854048e62a9b0886a9c2fc0925e3
Isa Abutaa <isaabutaa@gmail.com>
2021-01-28 10:28:30 -0500

    added css quiz

--- lev1/mid-assessment/app.js
+++ lev1/mid-assessment/app.js
@@ -1,14 +1,62 @@
-const form = document.form
-const firstName = form.firstName
-const lastName = form.lastName
-const list = document.getElementById("list")
+// Objective: add list items to the DOM by using the form
 
-form.addEventListener("submit", function(e) {
-    e.preventDefault()
 
-    const name = document.createElement("li")
-    name.textContent = firstName.value + " " + lastName.value
-    list.append(name)
-    firstName.value = ""
-    lastName.value = ""
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const form = document.form
+// const firstName = form.firstName
+// const lastName = form.lastName
+// const list = document.getElementById("list")
+
+// form.addEventListener("submit", function(e) {
+//     e.preventDefault()
+
+//     const name = document.createElement("li")
+//     name.textContent = firstName.value + " " + lastName.value
+//     list.append(name)
+//     firstName.value = ""
+//     lastName.value = ""
+// })
