e6a768e0d1d2da45eb2f95c3e7d2f2e02e7e825f
Isa Abutaa <isaabutaa@gmail.com>
2021-01-31 12:01:15 -0500

    set profile and public routes as protected routes

--- lev6/token-auth-pt2/client/src/App.js
+++ lev6/token-auth-pt2/client/src/App.js
@@ -4,26 +4,40 @@ import Navbar from './components/Navbar.js'
 import Auth from './components/Auth.js'
 import Profile from './components/Profile.js'
 import Public from './components/Public.js'
+import ProtectedRoute from './components/ProtectedRoute.js'
 import {UserContext} from './context/UserProvider.js'
 
 export default function App(){
   const { token, logout, todos } = useContext(UserContext)
   return (
     <div className="app">
-      <Navbar logout={logout} />
+      {token && <Navbar logout={logout} />}
       <Switch>
         <Route 
           exact path="/" 
           render={()=> token ? <Redirect to="/profile" /> : <Auth />}
         />
-        <Route 
+        <ProtectedRoute
+          path="/profile"
+          token={token}
+          component={Profile}
+          redirectTo="/"
+          todos={todos}
+        />
+        {/* <Route 
           path="/profile"
           render={() => <Profile todos={todos} />}
+        /> */}
+        <ProtectedRoute 
+          path="/public"
+          token={token}
+          component={Public}
+          redirectTo="/"
         />
-        <Route 
+        {/* <Route 
           path="/public"
           render={() => <Public />}
-        />
+        /> */}
       </Switch>
     </div>
   )
