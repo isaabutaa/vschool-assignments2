85228ca0c8cec5fee5e6064994be2832df3bf9b4
Isa Abutaa <isaabutaa@gmail.com>
2021-02-06 16:58:53 -0500

    dropdown color theme event

--- lev1/dom-olympics-start/index.js
+++ lev1/dom-olympics-start/index.js
@@ -40,3 +40,41 @@ clearBtn.addEventListener("click", () => {
         messages[0].parentNode.removeChild(messages[0])
     }
 })
+
+// get drop-down theme to work
+
+const dropDown = document.getElementById("theme-drop-down")
+    
+dropDown.addEventListener("change", function(e) {
+    if(e.target.value === "theme-one") {
+        for(let i = 0; i < messages.length; i++) {
+            if(i % 2 === 0) {
+                messages[i].style.backgroundColor = "burlywood"
+                messages[i].style.color = "black"
+            } else {
+                messages[i].style.backgroundColor = "lightblue"
+                messages[i].style.color = "black"
+            }
+        }
+    } else if(e.target.value === "theme-two") {
+        for(let i = 0; i < messages.length; i++) {
+            if(i % 2 === 0) {
+                messages[i].style.backgroundColor = "red"
+                messages[i].style.color = "white"
+            } else {
+                messages[i].style.backgroundColor = "black"
+                messages[i].style.color = "white"
+            }
+        }
+    } else if(e.target.value === "theme-three") {
+        for(let i = 0; i < messages.length; i++) {
+            if(i % 2 === 0) {
+                messages[i].style.backgroundColor = "orange"
+                messages[i].style.color = "white"
+            } else {
+                messages[i].style.backgroundColor = "purple"
+                messages[i].style.color = "white"
+            }
+        }
+    }
+})
